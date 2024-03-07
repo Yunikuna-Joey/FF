f9a730a3fa47a411a37a7ed0e82e23168946e970
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-07 14:11:01 -0800

    [update] redirection is working
    used non-depcrecated methods for navigation...

--- FF/FF/LoginView.swift
+++ FF/FF/LoginView.swift
@@ -3,6 +3,7 @@
 //  FF
 //
 //
+// seems like there can be conflicting behaviors... alert box overrules view redirection
 
 import SwiftUI
 
@@ -10,62 +11,64 @@ struct LoginView: View {
     // @state is for variables within one specfic view
     @State private var username: String = ""
     @State private var password: String = ""
-    @State private var activeAlert: Bool = false
-    @State private var alertMsg: String = ""
+    
+    // This is to keep track of user status,,, either logged in or not
+    @State private var status: Bool = false
     
     var body: some View {
-        VStack {
-            TextField("Username", text: $username)
-                // padding is to provide space around an UI element
-                .padding()
-                .background(Color.gray.opacity(0.33))
-                .cornerRadius(5)
-                // apply horizontal padding of 50 points
-                .padding(.horizontal, 50)
-            
-            SecureField("Password", text: $password)
-                .padding()
-                .background(Color.gray.opacity(0.33))
-                .cornerRadius(5)
-                .padding(.horizontal, 50)
-            
-            // the action is the if statement checking
-            Button(action: {
-                // success login functionality
-                if self.username == "your_username" && self.password == "your_password" {
-                    self.alertMsg = "Login Success!"
-                    self.activeAlert = true
-                }
-                // failed login functionality
-                else {
-                    self.alertMsg = "Login Error!"
-                    self.activeAlert = true
-                }
-            }) {
-                Text("Login")
-                    // text color
-                    .foregroundStyle(.white)
+        // Navigation Stack provides a storyline within our elements.. indicating that another screen will be loaded
+        NavigationStack {
+            VStack {
+                TextField("Username", text: $username)
+                    // padding is to provide space around an UI element
                     .padding()
-                    // size of the button customized
-                    .frame(width: 200, height: 50)
-                    .background(Color.blue)
+                    .background(Color.gray.opacity(0.33))
                     .cornerRadius(10)
-            }
-            // this is the dyanmic creation of the dialogue box
-            .alert(isPresented: $activeAlert) {
-                // bolded title
-                Alert(title: Text("Login"),
-                // status message
-                message: Text(alertMsg),
-                // dynamically create the OK button 
-                dismissButton: .default(Text("OK")))
-            }
-        }
-        // apply space from the top
-        .padding(.top, 100)
-    }
+                    // apply horizontal padding of 50 points
+                    .padding(.horizontal, 50)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                
+                // the action is the if statement checking
+                Button(action: {
+                    // success login functionality
+                    if self.username == "123" && self.password == "123" {
+                        self.status = true
+                    }
+                    // failed login functionality
+                    else {
+        
+                        self.status = false
+                    }
+                }) {
+                    Text("Login")
+                        // text color
+                        .foregroundStyle(.white)
+                        .padding()
+                        // size of the button customized
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .navigationDestination(isPresented: $status) {
+                    ContentView()
+                }
+            } // end of VStack here
+            
+            
+            .navigationTitle("Welcome to FF")
+            
+            
+            
+        } // end of NavigationStack here
+        
+    } // end of body here
     
-}
+} // end of structure here
 
 #Preview {
     LoginView()
