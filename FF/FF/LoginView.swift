fa8b541d8be9f064fd8099ea2dff35f3fc86c035
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-07 01:56:57 -0800

    [wip] login view
    basic view configured... work on colors and actual connection of database for further practicality

--- FF/FF/LoginView.swift
+++ FF/FF/LoginView.swift
@@ -28,7 +28,41 @@ struct LoginView: View {
                 .background(Color.gray.opacity(0.33))
                 .cornerRadius(5)
                 .padding(.horizontal, 50)
+            
+            // the action is the if statement checking
+            Button(action: {
+                // success login functionality
+                if self.username == "your_username" && self.password == "your_password" {
+                    self.alertMsg = "Login Success!"
+                    self.activeAlert = true
+                }
+                // failed login functionality
+                else {
+                    self.alertMsg = "Login Error!"
+                    self.activeAlert = true
+                }
+            }) {
+                Text("Login")
+                    // text color
+                    .foregroundStyle(.white)
+                    .padding()
+                    // size of the button customized
+                    .frame(width: 200, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            // this is the dyanmic creation of the dialogue box
+            .alert(isPresented: $activeAlert) {
+                // bolded title
+                Alert(title: Text("Login"),
+                // status message
+                message: Text(alertMsg),
+                // dynamically create the OK button 
+                dismissButton: .default(Text("OK")))
+            }
         }
+        // apply space from the top
+        .padding(.top, 100)
     }
     
 }
