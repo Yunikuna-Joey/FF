8dfdd556604219a57610fe291a5e7dce2690dd14
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-08 15:19:58 -0800

    [update] overall status layout
    layout seems to be good... change as necessary... dynamic information load another time

--- FF/FF/HomeView.swift
+++ FF/FF/HomeView.swift
@@ -15,7 +15,10 @@ struct HomeView: View {
             VStack(spacing: 10) {
                 // for loop, to iterate through each status update element [dynamic way of creating dummy data]
                 ForEach(1..<10) { index in
-                    StatusUpdateView(username: "User \(index)", timeAgo: "\(index)m ago", status: "This is the status update text. It can be as long as you want.")
+                    StatusUpdateView(
+                        username: "User \(index)",
+                        timeAgo: "\(index)m ago",
+                        status: "Checked in at this location (5 miles away)")
                 }
             }
             // create some extra spacing
@@ -37,21 +40,57 @@ struct StatusUpdateView: View {
         VStack(alignment: .leading, spacing: 10) {
             // stacked left to right
             HStack(spacing: 10) {
-                // image on the left
+                // profile image on the left
                 Image(systemName: "person.circle.fill")
                     .resizable()
                     .frame(width: 30, height: 30)
                     .foregroundColor(.blue)
+                
                 // username text next to image
                 Text(username)
                     .font(.headline)
+                
                 // space between
                 Spacer()
+                
                 // time that message was 'created'
                 Text(timeAgo)
                     .font(.caption)
                     .foregroundColor(.gray)
             }
+            HStack {
+                Text("Bubble 1")
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                    )
+                    // experiment with font, [caption, none or body, subheadline, footnote, callout]
+                    .font(.callout)
+                
+                Text("Bubble 2")
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                    )
+                    // experiment with font, [caption, none or body, subheadline, footnote, callout]
+                    .font(.callout)
+                
+                Text("Bubble 3")
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                    )
+                    // experiment with font, [caption, none or body, subheadline, footnote, callout]
+                    .font(.callout)
+            }
+            
+
             // below the left => right will be the actual status
             Text(status)
                 .font(.body)
