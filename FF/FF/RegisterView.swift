5c6f0717b9dd3d78705fb3d9e7b765410bc1263e
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-07 17:28:14 -0800

    [update] registration
    registration view is halfway done... need to work on view2 for registration as it will have the username and password portions

--- FF/FF/RegisterView.swift
+++ FF/FF/RegisterView.swift
@@ -7,8 +7,67 @@
 import SwiftUI
 
 struct RegisterView: View {
+    // CONSTANTS
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var error: Bool = false
+    @State private var cont: Bool = false
+    
     var body: some View {
-        Text("This is going to hold the register view")
+        NavigationStack {
+            VStack {
+                TextField("First Name", text: $firstName)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                
+                TextField("Last Name", text: $lastName)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                    .padding(.top, 10)
+                
+                TextField("Email", text: $email)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                    .padding(.top, 10)
+                
+                // Essentially an 'Error Field' implementation
+                if error {
+                    Text("Error: That email has already been used")
+                        .foregroundColor(.red)
+                        .padding(.top, 10)
+                }
+                
+                Button(action: {
+                    if email == "123" {
+                        error = true
+                    }
+                    
+                    else {
+                        cont = true
+                    }
+                    
+                }) {
+                    Text("Continue")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(width: 150, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.top, 25)
+                }
+                .navigationDestination(isPresented: $cont) {
+                    RegisterView2()
+                        .navigationBarBackButtonHidden(true)
+                }
+            }
+        }
     }
 }
 
---
+++ FF/FF/RegisterView2.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterView2.swift
+//  FF
+//
+//
+
+import SwiftUI
+
+struct RegisterView2: View {
+    var body: some View {
+        Text("Hello, Worlds!")
+    }
+}
+
+#Preview {
+    RegisterView2()
+}
