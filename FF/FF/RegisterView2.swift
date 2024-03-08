0ade8f49e751781d518bdfffde3420dec157b326
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-07 19:39:03 -0800

    [update] registration
    view2 => username and password seems to be working as intended... need more check(s) and visuals

--- FF/FF/RegisterView2.swift
+++ FF/FF/RegisterView2.swift
@@ -7,8 +7,84 @@
 import SwiftUI
 
 struct RegisterView2: View {
+    // CONSTANTS
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var confirmation: String = ""
+    
+    @State private var status: Bool = false
+    @State private var error: Bool = false
+    @State private var error2: Bool = false
+    
     var body: some View {
-        Text("Hello, Worlds!")
+        NavigationStack {
+            VStack {
+                TextField("Username", text: $username)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                    .padding(.top, 10)
+                
+                SecureField("Confirm Password", text: $confirmation)
+                    .padding()
+                    .background(Color.gray.opacity(0.33))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 50)
+                    .padding(.top, 10)
+                
+                // matching password
+                if error {
+                    Text("Error: Passwords do not match!")
+                        .foregroundColor(.red)
+                        .padding(.top, 10)
+                }
+                
+                // password length checker
+                if error2 {
+                    Text("Error: Password does not meet the minimum length requirements")
+                        .foregroundColor(.red)
+                        .padding(.top, 10)
+                }
+                
+                Button(action: {
+                    if self.password != self.confirmation {
+                        self.error2 = false
+                        self.error = true
+                    }
+                    
+                    else if confirmation.count < 8 || password.count < 8 {
+                        self.error = false
+                        self.error2 = true
+                    }
+                    
+                    else {
+                        self.error = false
+                        self.error2 = false
+                        self.status = true
+                    }
+                    
+                }) {
+                    Text("Register!")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(width: 150, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.top, 25)
+                }
+                .navigationDestination(isPresented: $status) {
+                    LoginView()
+                        .navigationBarBackButtonHidden(true)
+                }
+            }
+        }
     }
 }
 
