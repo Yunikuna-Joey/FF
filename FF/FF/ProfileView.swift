8efa3fedc2480823456bc091d12d78c20915bc27
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-11 01:28:01 -0700

    [wip] profileview
    cover photo and pfp are dynamic set for different screen sizes
    => need to implement more ui elements

--- FF/FF/ProfileView.swift
+++ FF/FF/ProfileView.swift
@@ -7,8 +7,36 @@
 import SwiftUI
 
 struct ProfileView: View {
+    // need to implement scroll view
+    
     var body: some View {
-        Text("Screen 5")
+        ZStack {
+            // Cover photo background
+            Image("Car")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity)
+                .frame(height: 200)
+                .clipped()
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                // Profile image overlaid on the bottom half of the cover photo
+                Image("car2")
+                    .resizable()
+                    .frame(width: 200, height: 150)
+                    .foregroundColor(.blue)
+                    .clipShape(Circle())
+                    .offset(y: 75) // Offset the profile image downwards
+                
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 200)
+            
+            
+            
+        } // end of ZStack
+        Spacer()
     }
 }
 
