f9580c114c065b9e9e8fcabf8f7c9cec1a2c4155
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-13 01:44:47 -0700

    [update] profileView3
    today's date is highlighted
    => still need dynamic creation of months
    => still need to be able to interact with dates

--- FF/FF/ProfileView3.swift
+++ FF/FF/ProfileView3.swift
@@ -8,14 +8,16 @@ import SwiftUI
 
 struct CalendarDayView: View {
     let day: Int
+    let selection: Bool
+    let isToday: Bool
     
     var body: some View {
         if day > 0 {
             Text("\(day)")
                 .frame(width: 30, height: 30)
-                .background(Color.clear)
+                .background(selection ? Color.blue : (isToday ? Color.yellow : Color.clear))
                 .clipShape(Circle())
-                .foregroundStyle(.primary)
+                .foregroundColor(selection ? .white : (isToday ? .black : .primary))
         }
         else {
             Text("")
@@ -28,14 +30,18 @@ struct CalendarDayView: View {
 
 struct ProfileView3: View {
     let total = 31
+    // [hardcoded for March,,, revise]
     let start = 5
+    let today = Calendar.current.component(.day, from: Date())
     
     var body: some View {
+        // [entire portion hardcoded,,, revise]
         let empty = Array(1..<start)
         let monthDays = Array(1...total)
         
         VStack {
             LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+                // gets the days of the week
                 ForEach(1...7, id: \.self) { day in
                     Text(DateFormatter().shortWeekdaySymbols[day % 7])
                         .frame(width: 30, height: 30)
@@ -43,16 +49,18 @@ struct ProfileView3: View {
                         .foregroundColor(.primary)
                 }
             }
-            
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
-                   ForEach(empty, id: \.self) { _ in
-                       CalendarDayView(day: 0)
-                   }
 
-                   ForEach(monthDays, id: \.self) { day in
-                       CalendarDayView(day: day)
-                   }
-               }
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+                // gets us the empty days of the month
+                ForEach(empty, id: \.self) { _ in
+                   CalendarDayView(day: 0, selection: false, isToday: false)
+                }
+                // gets all of the days and marks which one is 'today'
+                ForEach(monthDays, id: \.self) { day in
+                    let isToday = day == today
+                    CalendarDayView(day: day, selection: false, isToday: isToday)
+                }
+           }
         }
         
     }
