ab2dfeb8d5a90d3a3f5102780f3d83a60979acdb
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-17 21:25:35 -0700

    Update User.swift
    more comments

--- FF/FF/Data Model/User.swift
+++ FF/FF/Data Model/User.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-// User Data Model
+// User Data Model [may need to switch for data model file for viewing visually]
 struct User: Identifiable, Codable {            // codable turns JSON => data model object
     let id: String
     let username: String
