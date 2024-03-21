fbb5100b9e8d23c1fbd2d071e5bb4dfc0ac5f9fa
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-20 21:54:53 -0700

    [update]
    location data model

---
+++ FF/FF/Data Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  FF
+//
+//
+
+import Foundation
+
+// continue building for location data
+struct Location: Identifiable, Codable {
+    let id: String
+    let name: String
+    let address: String
+}
