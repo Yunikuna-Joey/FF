1656e062cc50ee170a1c94858dedaed1d21e4144
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-25 20:52:51 -0700

    [update]
    => reading over prev code

--- FF/FF/Core/Authentication/ViewModel/LocationManager.swift
+++ FF/FF/Core/Authentication/ViewModel/LocationManager.swift
@@ -9,29 +9,32 @@ import CoreLocation
 class LocationManager: NSObject, ObservableObject {
     // handle changes
     private let manager = CLLocationManager()
-    //
+    // Listens for changes in the user location
     @Published var userLocation: CLLocation?
     
     // ***optional allow for location manager to be accessed throughout the entire app
     static let shared = LocationManager()
     
+    // overwrites the default NSObject init
     override init() {
         super.init()
+        // refer to the LocationManager for best location
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.startUpdatingLocation()
     }
-        
+    
+    // provides the dialogue prompt as a function
     func requestLocation() {
         manager.requestWhenInUseAuthorization()
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
+    // function that will trigger when the location services permission changes
     func manageLocation(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        
         switch status {
-            
+            // the different cases here
             case .notDetermined:
                 print("[DEBUG]: Location not determined")
             case .restricted:
@@ -46,7 +49,7 @@ extension LocationManager: CLLocationManagerDelegate {
                 break
         }
     }
-    
+    // updates the location with last known 
     func manageLocation(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else {return}
         self.userLocation = location
