f5ff5d08d2c4d1a26fbd3246db9230e5232218af
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-04-03 20:54:07 -0700

    [update]
    => Attempt to create a settings icon in the top right for various device types

--- FF/FF/Core/Profile/ProfileView.swift
+++ FF/FF/Core/Profile/ProfileView.swift
@@ -11,10 +11,12 @@ struct ProfileView: View {
     @EnvironmentObject var viewModel: AuthView
     @State private var current: Tab = .status
     
+    // iterate through the different tabs
     func currSelection(_ tab:Tab) -> Bool {
         return current == tab
     }
     
+    // create cases for tabs
     enum Tab {
         case status
         case images
@@ -25,157 +27,143 @@ struct ProfileView: View {
     let username = "Womp Womp"
     let value1 = 25
     
+    let screenSize = UIScreen.main.bounds.size
+    
     var body: some View {
-        if let user = viewModel.currentSession {
-            GeometryReader { geometry in
-                let screenHeight = geometry.size.height
-                let screenWidth = geometry.size.width
-                
-                VStack {
-                    // Cover Photo
-                    Image("Car")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: screenHeight * 0.35)
-                        .clipped()
-                    
-                    // profile picture
-                    Image("car2")
-                        .resizable()
-                        .frame(width: 200, height: 150)
-                        .clipShape(Circle())
-                        .offset(y: -100) // Adjust this multiplier as needed
-                    
-                    // username [TEMPORARY HSTACK]
-                    HStack {
-                        Text("@\(user.username)")
-                            .font(.headline)
-                            .offset(y: -screenHeight * 0.12)
-                        
-                        Button(action: {
-                            viewModel.signOut()
-                            print("After the viewmodel signout button")
-                        }) {
-                            Image(systemName: "xbox.logo")
-                                .padding()
-                                .foregroundStyle(Color.blue)
-                        }
-                        
-                        Button(action: {
-                            Task {
-                                await viewModel.fetchAllUsers()
-                                print("After the view model fetch button")
-                            }
-                        }) {
-                            Image(systemName: "list.bullet.clipboard")
-                                .padding()
-                                .foregroundStyle(Color.blue)
-                        }
-                    }
-    //                Text("@\(username)")
-    //                    .font(.headline)
-    //                    .offset(y: -screenHeight * 0.12)
-                        
-                    // portion for user statistics
-                    HStack(spacing: screenWidth * 0.15) {
-                        // category 1
-                        VStack {
-                            Text("Check-Ins")
-                                .font(.headline)
-                            Text("\(value1)")
-                        }
-                        
-                        // category 2
-                        VStack {
-                            Text("Followers")
-                                .font(.headline)
-                            
-                            Text("\(value1)")
-                        }
-                        
-                        // category 3
-                        VStack {
-                            Text("Following")
-                                .font(.headline)
-                            
-                            Text("\(value1)")
-                        }
-                        
-                    }
-                    .offset(y: -screenHeight * 0.1)
-                    
-                    
-                    // icons for different views
-                    HStack(spacing: screenWidth * 0.25) {
-                        Button(action: {
-                            current = .status
-                        }) {
-                            Image(systemName: "person.fill")
-                                .padding()
-                                .foregroundStyle(Color.blue)
-                                .overlay(
-                                    Rectangle()
-                                        .frame(height: 2)
-                                        .offset(y: 20)
-                                        .foregroundStyle(currSelection(.status) ? .blue : .clear)
-                                )
-                        }
-                        
-                        Button(action: {
-                            current = .images
-                        }) {
-                            Image(systemName: "photo.fill")
-                                .padding()
-                                .foregroundStyle(Color.blue)
-                                .overlay(
-                                    Rectangle()
-                                        .frame(height: 2)
-                                        .offset(y: 20)
-                                        .foregroundStyle(currSelection(.images) ? .blue : .clear)
-                                )
-                        }
-                        
-                        Button(action: {
-                            current = .others
-                        }) {
-                            Image(systemName: "calendar")
-                                .padding()
-                                .foregroundStyle(Color.blue)
-                                .overlay(
-                                    Rectangle()
-                                        .frame(height: 2)
-                                        .offset(y: 20)
-                                        .foregroundStyle(currSelection(.others) ? .blue : .clear)
-                                )
-                        }
-                        
-                        
-                    } // end of hstack for the icons
-                    .foregroundColor(.blue)
-                    .offset(y: -screenHeight * 0.1)
-                    
-                    VStack {
-                        // logic for switching different views
-                        switch current {
-                        case .status:
-                            ProfileView1()
-                        case .images:
-                            ProfileView2()
-                        case .others:
-                            ProfileView3()
-                        }
-                    }
-                    .offset(y: -screenHeight * 0.1)
-                    .padding(.horizontal, 5)
-                    .frame(minHeight: screenHeight * 0.45) // ensure that all content will be shown and not stuck under the safe area
-                        
-                    
-                } // end of VStack
-                .offset(y: -screenHeight * 0.1)
+        VStack {
+            // Cover Photo
+            Image("Car")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity)
+                .frame(height: screenSize.height * 0.30)
+                .clipped()
+            
+            // profile picture
+            Image("car2")
+                .resizable()
+                .frame(width: 200, height: 150)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                // [play with this offset value]
+                .offset(y: -100)
+            
+            // pushes the cover photo AND profile picture
+            Spacer()
+            
+            // Stack for username
+            HStack {
+                Text("@username")
+                    .font(.headline)
+                    // [play with this offset value]
             }
-        }
-    }
+            .offset(y: -screenSize.height * 0.12)
+            
+            // HStack for user statistics
+            HStack(spacing: screenSize.width * 0.15) {
+                // category 1
+                VStack {
+                    Text("Check-Ins")
+                        .font(.headline)
+                    Text("\(value1)")
+                }
+                // category 2
+                VStack {
+                    Text("Followers")
+                        .font(.headline)
+                    Text("\(value1)")
+                }
+                // category 3
+                VStack {
+                    Text("Following")
+                        .font(.headline)
+                    Text("\(value1)")
+                }
+            }
+            .offset(y: -screenSize.height * 0.10)
+            
+            // HStack for clickable icons to switch between the different tabs
+            HStack(spacing: screenSize.width * 0.25) {
+                // Button #1
+                Button(action: {
+                    current = .status
+                }) {
+                    Image(systemName: "person.fill")
+                        .padding()
+                        .foregroundStyle(Color.blue)
+                        .overlay(
+                            Rectangle()
+                                .frame(height: 2)
+                                .offset(y: 20)
+                                .foregroundStyle(currSelection(.status) ? .blue : .clear)
+                        )
+                }
+                
+                // Button #2
+                Button(action: {
+                    current = .images
+                }) {
+                    Image(systemName: "photo.fill")
+                        .padding()
+                        .foregroundStyle(Color.blue)
+                        .overlay(
+                            Rectangle()
+                                .frame(height: 2)
+                                .offset(y: 20)
+                                .foregroundStyle(currSelection(.images) ? .blue : .clear)
+                        )
+                }
+                
+                // Button #3
+                Button(action: {
+                    current = .others
+                }) {
+                    Image(systemName: "calendar")
+                        .padding()
+                        .foregroundStyle(Color.blue)
+                        .overlay(
+                            Rectangle()
+                                .frame(height: 2)
+                                .offset(y: 20)
+                                .foregroundStyle(currSelection(.others) ? .blue : .clear)
+                        )
+                }
+            }
+            .foregroundStyle(Color.blue)
+            .offset(y: -screenSize.height * 0.10)
+            
+            // VStack for the actual different tab views
+            VStack {
+                switch current {
+                case .status:
+                    ProfileView1()
+                case .images:
+                    ProfileView2()
+                case .others:
+                    ProfileView3()
+                }
+            }
+            .offset(y: -screenSize.height * 0.10)
+            .padding(.horizontal, 5)
+            .frame(minHeight: screenSize.height * 0.45)
+        } // end of VStack
+        .overlay(
+            Button(action: {
+                print("Settings here for sign out and such")
+            }) {
+                Image(systemName: "gearshape.fill")
+                    .padding()
+                    .foregroundStyle(Color.blue)
+            }
+                .alignmentGuide(.top) { dimension in
+                    dimension[.top]
+                }
+                .alignmentGuide(.trailing) { dimension in
+                    dimension[.trailing]
+                }
+          
+        )
+        
+    } // end of body here
 }
 
 #Preview {
