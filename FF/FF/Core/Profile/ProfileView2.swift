3d50aefef60793ecb73906a61576ef240f1c1deb
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-04-04 12:29:49 -0700

    [update]
    => profileView2 will now have X by 2 grid [tentatively]
    => need to implement picture enlargement

--- FF/FF/Core/Profile/ProfileView2.swift
+++ FF/FF/Core/Profile/ProfileView2.swift
@@ -8,6 +8,9 @@ import SwiftUI
 
 // Images view
 struct ProfileView2: View {
+    // Determine the current device viewport dimensions
+    let screenSize = UIScreen.main.bounds.size
+    
     let imageArray = [
         "Car", 
         "car2",
@@ -20,23 +23,34 @@ struct ProfileView2: View {
         "car8",
         "car9"
     ]
-    let itemSize: CGFloat = (UIScreen.main.bounds.width - 40 - 20) / 3 - 10
     
     var body: some View {
+        let itemWidth: CGFloat = (screenSize.width) / 3
+        let itemHeight: CGFloat = (screenSize.height) * 0.10
+        
         ZStack(alignment: .bottom) {
             // enable scrolling behavior
             ScrollView {
                 // grid to hold the pictures
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: itemSize))]) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: itemWidth))]) {
                     // iterate through the image array
                     ForEach(imageArray, id: \.self) {
                         imageName in Image(imageName)
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: itemSize + 32, height: itemSize)
+                            .frame(width: itemWidth + 50, height: itemHeight)
                             .cornerRadius(5)
+                            .padding()
                     }
                 }
+            }
+            
+            // ***[omit] this portion in prod
+            Button(action: {
+                print("This is image width \(itemWidth)")
+                print("This is image height \(itemHeight)")
+            }) {
+                Text("Click me for more information")
             }
         } // end of ZStack
     }
