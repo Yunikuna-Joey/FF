4a32e08e8637196824a1439a26abe463dae1145f
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-04-08 13:53:37 -0700

    [update]
    => added a timestamp to the chat conversation
    => format looks to be good for now

--- FF/FF/Messages/IndividualChatView.swift
+++ FF/FF/Messages/IndividualChatView.swift
@@ -15,7 +15,9 @@ struct IndividualChatView: View {
     let username = "Testing Username"
     let longText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
     let shortText = "What are you hitting today?"
-   
+    
+    let timestamp = "1:40PM"
+    
     var body: some View {
         let screenSize = UIScreen.main.bounds.size
         
@@ -24,9 +26,15 @@ struct IndividualChatView: View {
             // Scroll View for message content `
             ScrollView(showsIndicators: false) {
                 VStack {
-
+                    
                     // This is the case for current user
                     HStack {
+                        // timestamp
+                        Text(timestamp)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                            .padding()
+                        
                         // Push to the right
                         Spacer()
                         
@@ -35,7 +43,7 @@ struct IndividualChatView: View {
                             Text(shortText)
                                 .padding()
                         }
-                        .frame(maxWidth: 256)           // needs to be different for different screen sizes
+                        /*.frame(maxWidth: 256)    */       // needs to be different for different screen sizes
                         .background(Color.blue)
                         .cornerRadius(25)
                         
@@ -61,9 +69,15 @@ struct IndividualChatView: View {
                             Text(shortText)
                                 .padding()
                         }
-                        .frame(maxWidth: 256)
+//                        .frame(maxWidth: 256)
                         .background(Color.gray)
                         .cornerRadius(25)
+                        
+                        // timestamp
+                        Text(timestamp)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                            .padding()
                         
                         // push to the left
                         Spacer()
@@ -85,6 +99,20 @@ struct IndividualChatView: View {
             
         }
         .navigationTitle("\(username)")
+    }
+    
+    // helper function to achieve time stamps associated with status's
+    func formatTimeAgo(from date: Date) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.year, .month, .weekOfMonth, .day, .hour, .minute, .second]
+        formatter.unitsStyle = .full
+        formatter.maximumUnitCount = 1
+        
+        guard let formattedString = formatter.string(from: date, to: Date()) else {
+            return "Unknown"
+        }
+        
+        return formattedString + " ago"
     }
 }
 
