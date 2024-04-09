087aa1f2ba04e0a0946860edd9a20ca0147e371e
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-04-09 12:38:02 -0700

    [update]
    => test view for searching users

--- FF/FF/Components/SearchView.swift
+++ FF/FF/Components/SearchView.swift
@@ -13,21 +13,98 @@ struct SearchView: View {
     
     @State private var searchText: String = ""
     
+    let username = "List User 1"
+    let bio = "Testing short bio here"
+    let following = 12
+    
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             // enable scrolling behavior
-            ScrollView {
-                // grid to hold the pictures
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: itemSize))]) {
-                    // iterate through the image array
-                    ForEach(imageArray, id: \.self) {
-                        imageName in Image(imageName)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: itemSize + 32, height: itemSize)
-                            .cornerRadius(5)
+            ScrollView(showsIndicators: false) {
+                // if the search bar is empty
+                if searchText.isEmpty {
+                    // grid to hold the pictures
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: itemSize))]) {
+                        // iterate through the image array
+                        ForEach(imageArray, id: \.self) {
+                            imageName in Image(imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: itemSize + 32, height: itemSize)
+                                .cornerRadius(5)
+                        }
                     }
                 }
+                
+                // if it is not empty
+                else {
+                    VStack {
+                        
+                        HStack {
+                            // image on top
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .padding(.leading, 20)
+                                .padding(.top, 10)
+                            
+                            // username || can add badges underneath to showcase
+                            Text(username)
+                                .font(.headline)
+                            
+                            // push to the left
+                            Spacer()
+                            
+                            // Follow and unfollow button || need logic to toggle between Follow and Unfollow
+                            Button(action: {
+                                print("Follow / unfollow button")
+                            }) {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundStyle(Color.green)
+                                    .overlay(
+                                        Text("Follow")
+                                            .foregroundStyle(Color.white)
+                                            .padding(4)
+                                    )
+                                    .frame(width: 100, height: 30)
+                                    .padding()
+                            }
+                        }
+                        
+                        // horizontal row of 3 most recent images
+                        HStack {
+                            Image("Car")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 100, height: 100)
+                                .clipped()
+                                .cornerRadius(10)
+                            
+                            Image("car2")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 100, height: 100)
+                                .clipped()
+                                .cornerRadius(10)
+                            
+                            Image("car3")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 100, height: 100)
+                                .clipped()
+                                .cornerRadius(10)
+                        }
+                        .padding()
+                        
+                    } // vstack for one card
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray.opacity(0.5))
+                    )
+                    .padding()
+                }
+
             }
             
             // search bar
@@ -40,6 +117,30 @@ struct SearchView: View {
                 .padding(.bottom)
                 .frame(maxWidth: 500 ) // Set maximum width
         } // end of ZStack
+    }
+}
+
+struct listUserProfiles: View {
+    let username: String
+    let bio: String
+    let followers: Int
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundStyle(Color.blue)
+            
+            VStack {
+                Text(username)
+                    .font(.headline)
+                
+                Text(bio)
+                    .font(.caption)
+            }
+        }
+        .padding()
     }
 }
 
